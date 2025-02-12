b4a907a06a27cbeefc5e2bf27983f917489b4d24
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-02-13 00:25:21 +0530

    Create sphere.java

---
+++ Week 2/Practical 4/sphere.java
@@ -0,0 +1,11 @@
+public class Sphere{
+
+	public void getSphereVolume(double radius){
+		
+
+		double answer=(4.0/3.0)*Math.PI*Math.pow(radius,3);
+		System.out.println("volume of the Sphere is: "+ answer);
+
+		
+	}
+}
