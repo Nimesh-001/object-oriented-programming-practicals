756595480ca2e3b9daf909a3fad5d0217e604a75
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-02-13 00:25:51 +0530

    Create cone.java

---
+++ Week 2/Practical 4/cone.java
@@ -0,0 +1,6 @@
+public class Cone {
+    	public void getConeVolume(double radius, double height) {
+        double ans2=(1.0 / 3.0) * Math.PI * Math.pow(radius, 2) * height;
+	System.out.println("volume of cone is: "+ans2);
+    }
+}
