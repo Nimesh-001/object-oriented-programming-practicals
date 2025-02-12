5b7ae39a802773f54d3de272fe6bb4d818d8b2ef
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-02-13 00:26:29 +0530

    Create myshape.java

---
+++ Week 2/Practical 4/myshape.java
@@ -0,0 +1,6 @@
+public class Myshape {
+    	public void getMyShapeVolume(double radius) {
+        double ans3= 2 * Math.PI * Math.pow(radius, 3);
+	System.out.println("volume of myshape is: "+ans3);
+    }
+}
