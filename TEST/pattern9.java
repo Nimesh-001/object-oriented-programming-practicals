ee1435c1b04fb9597aa5e9b95649e5c5438d888f
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-02-17 15:05:56 +0530

    Create pattern9.java

---
+++ TEST/pattern9.java
@@ -0,0 +1,18 @@
+public class pattern9{
+
+	public static void main(String args[]){
+		for(int i=5;i>=1;i--){
+			for(int sp=1;sp<=(5-i);sp++){
+				System.out.print(" ");
+			}
+
+			for(int j=1;j<=(2*i-1);j++){
+				System.out.print("*");
+			}
+
+		System.out.println();
+		}
+
+		
+	}
+}
