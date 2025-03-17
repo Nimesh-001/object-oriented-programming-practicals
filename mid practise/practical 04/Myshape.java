2d2dab5d76c67f69ef30d797ce25aea877be2ada
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-03-18 01:18:59 +0530

    Create Myshape.java

---
+++ mid practise/practical 04/Myshape.java
@@ -0,0 +1,8 @@
+public class Myshape {
+    public double getmyshapevolume(double radius){
+
+        double ans3=(4/3)*Math.PI*Math.pow(radius,3);
+        System.out.println("voume of the cone is: "+ans3);
+        return ans3;
+    }
+}
