63b6ad89d09f9813c37d7ce10db84446fd0d6af0
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-03-18 03:15:25 +0530

    Create Remunerationdemo.java

---
+++ mid practise/practical 08/question 2/Remunerationdemo.java
@@ -0,0 +1,8 @@
+public class Remunerationdemo {
+    public static void main(String[] args) {
+        Remuneration remuneration=new Remuneration();
+
+        remuneration.calcsalary();
+        remuneration.printsalary();
+    }
+}
