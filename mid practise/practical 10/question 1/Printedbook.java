aa8dbe2164e31aadef1aa411e72d923bf3d45525
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-03-18 14:00:03 +0530

    Create Printedbook.java

---
+++ mid practise/practical 10/question 1/Printedbook.java
@@ -0,0 +1,13 @@
+public class Printedbook extends Book {
+    public Printedbook(String title, String author) {
+        super(title, author);
+    }
+
+    @Override
+    public String getdiscription() {
+        return super.getdiscription();
+    }
+
+
+
+}
