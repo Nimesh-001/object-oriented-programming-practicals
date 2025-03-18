1e9e8cbcd8d12a55261dd5f2d9a01918fe0ba8b9
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-03-18 14:00:48 +0530

    Create Ebook.java

---
+++ mid practise/practical 10/question 1/Ebook.java
@@ -0,0 +1,11 @@
+public class Ebook extends Book {
+
+    public Ebook(String title, String author) {
+        super(title, author);
+    }
+
+    @Override
+    public String getdiscription() {
+        return super.getdiscription();
+    }
+}
