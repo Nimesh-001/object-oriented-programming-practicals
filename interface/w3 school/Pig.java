681b99cfc306db0b7c0c55f5bb5df3ace6f3d939
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-04 19:31:11 +0530

    Create Pig.java

---
+++ interface/w3 school/Pig.java
@@ -0,0 +1,12 @@
+public class Pig implements Animal {
+
+    @Override
+    public void animalsound() {
+        System.out.println("the pig says: we weee..");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("zzzzz.......");
+    }
+}
