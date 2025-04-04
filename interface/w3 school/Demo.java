21ddf5a8ad7a520e27dbc7faa11b479c85791321
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-04 19:42:11 +0530

    Create Demo.java

---
+++ interface/w3 school/Demo.java
@@ -0,0 +1,11 @@
+public class Demo implements Firstinterface,Secondinterface{
+    @Override
+    public void mymethod() {
+        System.out.println("first interface......");
+    }
+
+    @Override
+    public void myothermethod() {
+        System.out.println("second interface......");
+    }
+}
