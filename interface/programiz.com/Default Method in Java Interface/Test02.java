2e89622d18931e23b575bb52ff1e150937818c37
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-04 22:45:43 +0530

    Create Test02.java

---
+++ interface/programiz.com/Default Method in Java Interface/Test02.java
@@ -0,0 +1,12 @@
+public class Test02 {
+    public static void main(String[] args) {
+        B b=new B();
+        b.getarea();
+        b.getsides();
+
+        C c=new C();
+        c.getarea();
+        c.getsides();
+
+    }
+}
