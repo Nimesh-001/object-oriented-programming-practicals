06d122eb1b4e8f496712ddd64ef09a28ca21f4a7
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-04 22:19:19 +0530

    Create Rectangle.java

---
+++ interface/programiz.com/Rectangle.java
@@ -0,0 +1,7 @@
+public class Rectangle implements Polygon{
+    @Override
+    public void getarea(int length, int breadth) {
+        System.out.println("the are of the rectangle is: "+ (length*breadth));
+
+    }
+}
