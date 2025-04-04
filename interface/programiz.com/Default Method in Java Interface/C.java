46b254fd39142b9bc66c39e59693379b5fc41d36
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-04 22:45:11 +0530

    Create C.java

---
+++ interface/programiz.com/Default Method in Java Interface/C.java
@@ -0,0 +1,8 @@
+public class C implements A {
+    @Override
+    public void getarea() {
+        int length=5;
+        int area=length*length;
+        System.out.println("Area is:"+area);
+    }
+}
