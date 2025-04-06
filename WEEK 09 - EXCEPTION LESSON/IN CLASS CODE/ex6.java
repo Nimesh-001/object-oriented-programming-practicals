b008ed94550c0359b1442a1b146a17e8488cbf88
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-06 12:25:22 +0530

    Create ex6.java

---
+++ WEEK 09 - EXCEPTION LESSON/IN CLASS CODE/ex6.java
@@ -0,0 +1,21 @@
+public class unchecked {
+    public static void main(String[] args) {
+
+        System.out.println("start....");
+        int mynumber[] ={1,2,3,4,5};
+        try {
+            System.out.println(mynumber[7]);
+        } catch (NullPointerException e) {
+            //throw new RuntimeException(e);
+            System.out.println("null pointer....");
+            //System.out.println(e.fillInStackTrace());
+            //e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("array exception");
+        }catch (Exception e){
+            System.out.println("generic exception");
+        }
+
+        System.out.println("end...");
+    }
+}
