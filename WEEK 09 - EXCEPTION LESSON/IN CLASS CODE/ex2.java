2fc66ca0c959add9c88ee163197b82fb993a88c6
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-06 12:22:32 +0530

    Create ex2.java

---
+++ WEEK 09 - EXCEPTION LESSON/IN CLASS CODE/ex2.java
@@ -0,0 +1,15 @@
+public class unchecked {
+    public static void main(String[] args) {
+
+        System.out.println("start....");
+        int mynumber[] ={1,2,3,4,5};
+        try {
+            System.out.println(mynumber[7]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //throw new RuntimeException(e);
+            System.out.println(e);
+        }
+
+        System.out.println("end...");
+    }
+}
