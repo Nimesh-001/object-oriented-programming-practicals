eca1888d37bb6bf3e51430e8e78168c1c61df6ad
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-06 12:23:15 +0530

    Create ex3.java

---
+++ WEEK 09 - EXCEPTION LESSON/IN CLASS CODE/ex3.java
@@ -0,0 +1,17 @@
+public class unchecked {
+    public static void main(String[] args) {
+
+        System.out.println("start....");
+        int mynumber[] ={1,2,3,4,5};
+        try {
+            System.out.println(mynumber[7]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //throw new RuntimeException(e);
+            System.out.println(e.getMessage());
+            System.out.println(e.fillInStackTrace());
+            e.printStackTrace();
+        }
+
+        System.out.println("end...");
+    }
+}
