a274ad5281eb88817d9464a68e9e72b6e9bfbb38
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-08 18:59:36 +0530

    Create Check.java

---
+++ WEEK 09 - EXCEPTION LESSON/Lecture note/Check.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Check {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("abc.txt");
+    }
+}
