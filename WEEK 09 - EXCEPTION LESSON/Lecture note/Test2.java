62afca4e87e05f235669e8e04043d6f546394160
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-08 19:39:00 +0530

    Create Test2.java

---
+++ WEEK 09 - EXCEPTION LESSON/Lecture note/Test2.java
@@ -0,0 +1,11 @@
+public class Test2 extends Exception{
+    private double amount;
+
+    public Test2(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
