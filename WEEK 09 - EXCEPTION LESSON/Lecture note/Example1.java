65ffb9712fd4931a0a561c34f78f6c2647437872
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-08 19:27:41 +0530

    Create Example1.java

---
+++ WEEK 09 - EXCEPTION LESSON/Lecture note/Example1.java
@@ -0,0 +1,16 @@
+public class Example {
+    void checkage(int age){
+        if(age < 18){
+            throw new ArithmeticException("not eligible for vorting...");
+        }
+        else{
+            System.out.println("I am eligible for vorting...");
+        }
+
+    }
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.checkage(18);
+        System.out.println("end of program...");
+    }
+}
