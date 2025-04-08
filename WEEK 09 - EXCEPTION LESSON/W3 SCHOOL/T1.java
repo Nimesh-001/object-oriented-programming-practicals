b833e2da95de8443be7010ce89bbb1d7969cdfdc
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-08 19:50:47 +0530

    Create T1.java

---
+++ WEEK 09 - EXCEPTION LESSON/W3 SCHOOL/T1.java
@@ -0,0 +1,11 @@
+public class T1 {
+    public static void main(String[] args) {
+        int [] number={1,2,3};
+        System.out.println(number[10]);
+    }
+}
+
+//Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 3
+//	at T1.main(T1.java:4)
+
+//Process finished with exit code 1
