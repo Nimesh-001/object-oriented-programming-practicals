f1cae46a591e3f52628c5a9597187def39d9040d
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-08 19:54:08 +0530

    Create T2.java

---
+++ WEEK 09 - EXCEPTION LESSON/W3 SCHOOL/T2.java
@@ -0,0 +1,13 @@
+public class T2 {
+    public static void main(String[] args) {
+
+        try {
+            int [] number ={1,2,3};
+            System.out.println(number[10]);
+        } catch (Exception e) {
+            System.out.println("something went wrong....");
+        }
+
+
+    }
+}
