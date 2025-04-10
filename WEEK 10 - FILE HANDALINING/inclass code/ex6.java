75a4801beb603639c0b35b962612ac9bb8a5b7bd
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-11 00:08:57 +0530

    Create ex6.java

---
+++ WEEK 10 - FILE HANDALINING/inclass code/ex6.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Mycharacterreader {//to change name, right click ,refactor, rename
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\ICT_Common.LAB12-192\\Desktop\\tg1395\\student.txt");
+
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr =new FileReader(file);//first code this..  //check fr.read(homework)
+            br = new BufferedReader(fr);
+
+
+
+
+                String line = br.readLine();//second code this
+                while (line != null) {
+                    System.out.println(line);
+                    line = br.readLine();
+
+                }
+
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found"+e.getMessage());
+        } catch (IOException e) {
+            System.out.println("I/O Error"+e.getMessage());
+        }
+        finally {
+            try {
+                br.close();//code this first to add try catch
+            } catch (IOException e) {
+                System.out.println("Error closing stream..."+e.getMessage());
+            }
+        }
+    }
+}
