4f5997096ec76477b200aa044849f277c6f24c23
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-11 00:11:06 +0530

    Create ex9.java

---
+++ WEEK 10 - FILE HANDALINING/inclass code/ex9.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Mycharacterwriting {
+    public static void main(String[] args) {
+
+        File file=new File("C:\\Users\\ICT_Common.LAB12-192\\Desktop\\tg1395\\student.txt");
+        try {
+            FileWriter fw= new FileWriter(file,true);//without this true we cannot get the privious data.
+            BufferedWriter bw=new BufferedWriter(fw);
+            PrintWriter pw=new PrintWriter(bw,true);
+            pw.println("ICT004    NAMAL");
+
+
+            FileReader fr=new FileReader(file);
+            BufferedReader br=new BufferedReader(fr);
+
+            String line=br.readLine();
+            while(line!=null){
+                System.out.println(line);
+                line=br.readLine();
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("error"+e.getMessage());
+        }
+
+    }
+}
