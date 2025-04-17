e46d5fc523310a1ee041de5b99fbba8438a1db39
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-04-17 20:30:30 +0530

    Update form.java

--- WEEK-08 - Swing GUI/System2/form.java
+++ WEEK-08 - Swing GUI/System2/form.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form {
     private JPanel panel1;
@@ -8,13 +10,29 @@ public class form {
     private JButton CLEARButton;
 
 
+    public form() {
+        LOGINButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        CLEARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("form");
         frame.setContentPane(new form().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(800,400);
+        frame.setSize(900,400);
         frame.setLocationRelativeTo(null);
         frame.setTitle("Login form");
         frame.setResizable(false);
