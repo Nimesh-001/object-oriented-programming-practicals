0d554c5b8cec61cff1ab8e4202ec2d8bd6c1c6a9
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:20:14 +0530

    Create 2.java

---
+++ TEST/2.java
@@ -0,0 +1,11 @@
+public class Printnumdemo {
+
+    public void printnumbers(){
+        //int i=1;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Number : "+i);
+
+        }
+    }
+}
