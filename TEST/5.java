ef5f458ceb362725937204cbbd7bd28d02fdee73
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:21:54 +0530

    Create 5.java

---
+++ TEST/5.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Printnumdemo printnumdemo = new Printnumdemo();
+        printnumdemo.printnumbers("first");
+        printnumdemo.printnumbers("second");
+
+    }
+}
