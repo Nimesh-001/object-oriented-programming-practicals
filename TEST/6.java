5826de6eb4f8eefca22256fe7fbef8d88a5ba1d1
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:22:30 +0530

    Create 6.java

---
+++ TEST/6.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        Printnumdemo printnumdemo = new Printnumdemo();
+        printnumdemo.printnumbers("first");
+        printnumdemo.printnumbers("second");
+
+
+        Printnumdemo printnumdemo2 = new Printnumdemo();//create 2 object here..
+        printnumdemo2.printnumbers("first");
+        printnumdemo2.printnumbers("second");
+
+    }
+}
