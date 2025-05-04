950b8c80872d22266b0690b94e5ff18b3a132c23
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:21:26 +0530

    Create 4.java

---
+++ TEST/4.java
@@ -0,0 +1,11 @@
+public class Printnumdemo {
+
+    public void printnumbers(String call){
+        //int i=1;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(call+"Number : "+i);
+
+        }
+    }
+}
