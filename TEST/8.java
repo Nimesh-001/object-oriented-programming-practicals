4d8213f38e2a04817b36031eb192a643e871b209
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:23:35 +0530

    Create 8.java

---
+++ TEST/8.java
@@ -0,0 +1,13 @@
+public class Printnumdemo {
+
+    public void printnumbers(String call){
+        //int i=1;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(call+"Number : "+i);
+
+        }
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+    }
+}
