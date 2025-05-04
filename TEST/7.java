00802c657e7c1352b37389b24ddc7e536ecda5ba
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-04 22:23:01 +0530

    Create 7.java

---
+++ TEST/7.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        Printnumdemo printnumdemo = new Printnumdemo();
+        printnumdemo.printnumbers("first");
+        printnumdemo.printnumbers("second");
+
+
+        Printnumdemo printnumdemo2 = new Printnumdemo();
+        printnumdemo2.printnumbers("third");
+        printnumdemo2.printnumbers("fourth");
+
+    }
+}
