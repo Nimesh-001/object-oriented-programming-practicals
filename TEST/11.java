22723f819b141666dd8d1f941664d3642946fb7e
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-06 23:24:12 +0530

    Create 11.java

---
+++ TEST/11.java
@@ -0,0 +1,62 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        //Printnumdemo printnumdemo = new Printnumdemo();
+       // printnumdemo.printnumbers("first");
+        //printnumdemo.printnumbers("second");
+
+
+        //Printnumdemo printnumdemo2 = new Printnumdemo();
+        //printnumdemo2.printnumbers("third");
+       // printnumdemo2.printnumbers("fourth");
+        
+        Mythread mythread = new Mythread();//oblect creating....
+        mythread.start();//starting.............
+
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //Printnumdemo printnumdemo = new Printnumdemo();
+       // printnumdemo.printnumbers("first");
+        //printnumdemo.printnumbers("second");
+
+
+        //Printnumdemo printnumdemo2 = new Printnumdemo();
+        //printnumdemo2.printnumbers("third");
+       // printnumdemo2.printnumbers("fourth");
+
+        Mythread mythread = new Mythread();//oblect creating....
+        mythread.start();//starting.............
+        mythread.start();
+        
+
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //Printnumdemo printnumdemo = new Printnumdemo();
+       // printnumdemo.printnumbers("first");
+        //printnumdemo.printnumbers("second");
+
+
+        //Printnumdemo printnumdemo2 = new Printnumdemo();
+        //printnumdemo2.printnumbers("third");
+       // printnumdemo2.printnumbers("fourth");
+
+        Mythread mythread = new Mythread();//oblect creating....
+        mythread.start();//starting.............
+        mythread.start();//cant stat same thread........run and seee....
+
+
+    }
+}
