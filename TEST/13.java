8df38652983a57dc03b397a7f81807f4eb6479a2
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-06 23:25:15 +0530

    Create 13.java

---
+++ TEST/13.java
@@ -0,0 +1,43 @@
+public class Mythread extends Thread {
+    public void run() {
+       // super.run();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Numbers : "+i);
+           /* try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+                
+            */
+
+        }
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //Printnumdemo printnumdemo = new Printnumdemo();
+       // printnumdemo.printnumbers("first");
+        //printnumdemo.printnumbers("second");
+
+
+        //Printnumdemo printnumdemo2 = new Printnumdemo();
+        //printnumdemo2.printnumbers("third");
+       // printnumdemo2.printnumbers("fourth");
+
+        //Mythread mythread = new Mythread();//oblect creating....
+        //mythread.start();//starting.............
+       // mythread.start();//cant stat same thread........run and seee....
+
+        Mythread mythread2 = new Mythread();
+        mythread2.start();
+
+
+    }
+}
