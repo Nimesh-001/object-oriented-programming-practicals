39a994ea357e400da3206925e72fc233b8f693e5
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-08 10:18:57 +0530

    Create 2.java

---
+++ WEEK 12/INCLASS/2.java
@@ -0,0 +1,21 @@
+public class singletondemo {
+    public static void main(String[] args) {
+
+
+        //singleton singleton = new singleton();
+
+        singleton.getInstance();
+//        singleton s = new singleton();
+//        s.printmsg();
+//
+//
+//        singleton s2 = new singleton();
+//        s2.printmsg();
+//
+//        singleton s3 = s;
+//
+//
+//        System.out.println(s.equals(s2));
+//        System.out.println(s.equals(s3));
+    }
+}
