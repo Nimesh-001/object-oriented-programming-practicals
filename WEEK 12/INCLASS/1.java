062d79c537d9e670ee66e1940ae8209a6470c323
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-08 10:18:27 +0530

    Create 1.java

---
+++ WEEK 12/INCLASS/1.java
@@ -0,0 +1,21 @@
+public class singleton {
+
+
+    //create single object
+    private  static singleton s =new singleton();
+
+
+    //make constructor private.. so outsiders cannot make instamces,
+    private singleton() {
+    }
+
+    //globle accesspoint
+    public  static singleton getInstance() {
+        return s;
+    }
+
+    public void printmsg(){
+
+        System.out.println("hello from sigleton");
+    }
+}
