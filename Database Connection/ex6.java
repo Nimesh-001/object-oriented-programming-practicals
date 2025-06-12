a1cf6190f30fcc9720ab6f5c673bf48a96171f8d
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-06-12 13:35:25 +0530

    Create ex6.java

---
+++ Database Connection/ex6.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+
+public class dm1 {
+    public static void main(String[] args) {
+        db1 db1 = new db1();
+        Connection conn = db1.getConnection();
+
+        if (conn != null) {
+            System.out.println("Connected to database successfully");
+        }
+        else {
+            System.out.println("Connection Failed");
+        }
+    }
+}
