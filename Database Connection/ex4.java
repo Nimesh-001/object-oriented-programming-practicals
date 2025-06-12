069aff931ddad19e7d289add0317a407a443d59f
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-06-12 13:05:44 +0530

    Update ex4.java

--- Database Connection/ex4.java
+++ Database Connection/ex4.java
@@ -4,5 +4,12 @@ public class demo {
     public static void main(String[] args) {
         dbconnector dbconnector = new dbconnector();
         Connection connection= dbconnector.getConnection();
+
+        if (connection != null) {
+            System.out.println("Connected to database successfully");
+        }
+        else {
+            System.out.println("Connection Failed");
+        }
     }
 }
